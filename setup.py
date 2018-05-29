fdf1058a52d6a2e35aeb7143d4e64c93483f3647
mnitchev <marionitchev@gmail.com>
2018-05-29 21:44:18 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""Setup the project."""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Finnr',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy>=1.11.1'],
+    tests_require=['coverage>=4.4.2', 'coveralls'],
+    author='Mario Nitchev',
+    author_email='mail@mail.com',
+    description='A wandering robot looking for his friend - a tennis ball.',
+    license='MIT',
+    keywords='simulation, raspberry pi, image recognision, opencv, robotics',
+    url='https://github.com/mnitchev/Finnr',
+    test_suite='tests'
+)
