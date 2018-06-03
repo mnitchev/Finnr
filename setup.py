65ceab828855e99d88444eeee9e99e2ffc56645b
mnitchev <marionitchev@gmail.com>
2018-06-03 18:08:46 +0300

    Add dependencies in setup.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='Finnr',
     version='0.1',
     packages=find_packages(),
-    install_requires=['numpy>=1.11.1'],
+    install_requires=['numpy>=1.11.1', 'opencv-python>=3.4.1.15', 'imutils>=0.4.6'],
     tests_require=['coverage>=4.4.2', 'coveralls'],
     author='Mario Nitchev',
     author_email='mail@mail.com',
