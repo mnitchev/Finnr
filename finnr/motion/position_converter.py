6f766edc4cfa21b202599e79a641e31f8dbe5b2e
mnitchev <marionitchev@gmail.com>
2018-06-03 18:07:36 +0300

    Add missing is_forward method in Motion. Implement convert in FramePositionMotionConverter.

--- finnr/motion/position_converter.py
+++ finnr/motion/position_converter.py
@@ -1,7 +1,14 @@
 class FramePositionMotionConverter(object):
     def __init__(self, frame_width, frame_height):
-        self.frame_width = frame_height
+        self.frame_width = frame_width
         self.frame_height = frame_height
 
     def convert(self, position):
-        pass  # fix me
+        halfw = self.frame_width / 2
+        steering = abs(halfw - position.x) / halfw
+        momentum = abs(self.max_size - position.size) / self.max_size
+        motion = Motion(momentum, steering)
+
+        if position.x < halfw:
+            motion.steering = -motion.steering
+        return motion
