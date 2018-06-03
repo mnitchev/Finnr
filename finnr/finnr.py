8fe27d3d39e983dc57312dd248961edbfb0cf5ef
mnitchev <marionitchev@gmail.com>
2018-06-04 00:17:25 +0300

    Fix FramePositionMotionConverter initialization.

--- finnr/finnr.py
+++ finnr/finnr.py
@@ -9,13 +9,13 @@ FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 SEEK_ROTATION_SPEED = 0.5
 ROTATION_DIRECTION = 1.0
-
+MAX_TARGET_SIZE = 100
 
 class Finnr(object):
     def __init__(self):
         regulators = [BackObstacleRegulator(3.0), FrontObstacleRegulator(3.0)]
         trafficChain = RegulatorChain(regulators)
-        converter = FramePositionMotionConverter(FRAME_WIDTH, FRAME_HEIGHT)
+        converter = FramePositionMotionConverter(FRAME_WIDTH, FRAME_HEIGHT, MAX_TARGET_SIZE)
         generator = RotatingMotionGenerator(SEEK_ROTATION_SPEED, ROTATION_DIRECTION)
 
         self.brain = Brain(trafficChain, converter, generator)
--- finnr/motion/position_converter.py
+++ finnr/motion/position_converter.py
@@ -1,7 +1,8 @@
 class FramePositionMotionConverter(object):
-    def __init__(self, frame_width, frame_height):
+    def __init__(self, frame_width, frame_height, max_size):
         self.frame_width = frame_width
         self.frame_height = frame_height
+        self.max_size = max_size
 
     def convert(self, position):
         halfw = self.frame_width / 2
