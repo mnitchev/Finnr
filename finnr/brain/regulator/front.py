59286c3595888b09b49bbb98c461e977d3f26c15
mnitchev <marionitchev@gmail.com>
2018-06-04 19:55:02 +0300

    Fix argument name

--- finnr/brain/regulator/front.py
+++ finnr/brain/regulator/front.py
@@ -2,8 +2,8 @@ class FrontObstacleRegulator(object):
     def __init__(self, minFrontDistance):
         self.minDistance = minFrontDistance
 
-    def regulate(self, motion, sensor_data):
-        if (motion.is_forward() and self.can_move_forward(sensorData)):
+    def regulate(self, motion, sensorData):
+        if motion.is_forward() and self.can_move_forward(sensorData):
             return motion.reverse()
         else:
             return motion.stop()
